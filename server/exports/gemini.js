f033fe8ee653463178244fc646daa5a797de4896
Shreehari-Acharya <shreehari.acharya.06@gmail.com>
2025-11-23 22:09:06 +0530

    New gemini exports

--- server/exports/gemini.js
+++ server/exports/gemini.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-console.log("GEMINI API KEY:", process.env.GEMINI_API_KEY);
+// console.log("GEMINI API KEY:", process.env.GEMINI_API_KEY);
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 })
